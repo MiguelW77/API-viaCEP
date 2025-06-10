c962d48200ad767c0936cd883cb99640a8599a47
Miguel Walisson <miguelwalisson098@gmail.com>
2025-06-10 20:49:09 -0300

    RhTec

---
+++ apiFuncionariosCargos/src/main/java/com/rhtec/api/apiFuncionariosCargos/ApiFuncionariosCargosApplication.java
@@ -0,0 +1,13 @@
+package com.rhtec.api.apiFuncionariosCargos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFuncionariosCargosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFuncionariosCargosApplication.class, args);
+	}
+
+}
---
+++ apiFuncionariosCargos/src/test/java/com/rhtec/api/apiFuncionariosCargos/ApiFuncionariosCargosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhtec.api.apiFuncionariosCargos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFuncionariosCargosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
